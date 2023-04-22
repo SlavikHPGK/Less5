b725a0a475c91bd97221676eeef90adb8d4a8441
SlavikHPGK <daniloslavik@gmail.com>
2023-04-22 09:59:34 +0300

    Print

---
+++ main.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+
+help(requests)
+
+URL = 'https://coinmarketcap.com/'
+
+r = requests.get(URL)
+
+data = r.text
+
+soup = BeautifulSoup(data, 'html.parser')
+
+a = soup.find_all('a', {"href":"/currencies/ethereum/markets/"
+})
+
+
+
+
+print(a[0].text)
+
+
+
